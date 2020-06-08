508ca6975387ebb4ae301e66495ee1d5d5b30188
JaewonKim <huhu420@naver.com>
2020-06-08 16:54:09 +0900

    [challenge] Power of Two
    - Add a Java solution(It will be fixed)

---
+++ algorithms/java/Challenge_Power_of_Two.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n == 1)
+            return true;
+        else if(n <= 0)
+            return false;
+        
+        while(n != 1) {
+            if(n%2 == 1) return false;
+            n = n/2;
+        }
+        return true;
+    }
+}
