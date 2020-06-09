5df4c7f5b4a49bbcf28b7afbf58e0625cdeee0f7
JaewonKim <huhu420@naver.com>
2020-06-09 16:48:24 +0900

    [challenge] Is Subsequence 2
    - Add fancy solution for Python

--- algorithms/python/Challenge_Is_Subsequence.py
+++ algorithms/python/Challenge_Is_Subsequence.py
@@ -1,14 +1,4 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        
-        sIdx = 0
-        tIdx = 0
-        while sIdx < len(s) and tIdx < len(t):
-            if t[tIdx] == s[sIdx]:
-                sIdx += 1
-            tIdx += 1
-            
-        if sIdx == len(s):
-            return True
-        
-        return False
+        iter_target = iter(target)
+        return all(char in iter_target for char in s)
