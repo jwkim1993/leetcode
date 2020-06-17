bd4880a42a3e8d3eb6cf8dec230c58b7a2554887
JaewonKim <huhu420@naver.com>
2020-06-17 17:58:32 +0900

    [challenge] Surrounded Regions
    - Add a Python solution
    - Modify the Java solution(upgrade performance)

--- algorithms/java/Challenge_Surrounded_Regions.java
+++ algorithms/java/Challenge_Surrounded_Regions.java
@@ -1,8 +1,7 @@
 class Solution {
-    Set<Coordi> visited = new HashSet<>();
     
     public void solve(char[][] board) {
-        if(board.length == 0 || board[0].length == 0)
+        if(board == null || board.length == 0 || board[0].length == 0)
             return;
         
         int rowLen = board.length;
@@ -20,55 +19,23 @@ class Solution {
         
         for(int i = 0 ; i < rowLen; ++i) {
             for(int j = 0 ; j < colLen; ++j) {
-                if(board[i][j] != 'X' && !visited.contains(new Coordi(i, j)))
+                if(board[i][j] == 'O')
                     board[i][j] = 'X';
+                if(board[i][j] == 'C')
+                    board[i][j] = 'O';
             }
         }
     }
     
     public void flip(char[][] board, int i, int j) {        
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length
-           || board[i][j] == 'X' || visited.contains(new Coordi(i, j)))
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O')
             return;
         
-        visited.add(new Coordi(i, j));
+        board[i][j] = 'C';
         
         flip(board, i-1, j);
         flip(board, i+1, j);
         flip(board, i, j-1);
         flip(board, i, j+1);
-    }
-    
-    class Coordi {
-        int i;
-        int j;
-        public Coordi(int i, int j) {
-            this.i = i;
-            this.j = j;
-        }
-        
-        @Override
-        public int hashCode() {
-            return i+j;
-        }
-        
-        @Override
-        public boolean equals(Object obj) {
-            Coordi coordi;
-            if(obj instanceof Coordi)
-                coordi = (Coordi) obj;
-            else
-                return false;
-            
-            if(this.i == coordi.i && this.j == coordi.j)
-                return true;
-            return false;
-        }
-        
-        @Override
-        public String toString() {
-            return String.format("(%d, %d)", i, j);
-        }
-        
     }
 }
