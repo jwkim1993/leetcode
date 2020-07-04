5481d07be08e28e9758e64d97a7406f40d84431e
JaewonKim <huhu420@naver.com>
2020-07-04 17:35:14 +0900

    [challenge] Get 1st speed code from leetcode
    - Get 1st speed code from leetcode
    - What a beautiful it is

--- algorithms/java/Jul.2020.challenge/Challenge_Ugly_Number_II.java
+++ algorithms/java/Jul.2020.challenge/Challenge_Ugly_Number_II.java
@@ -1,21 +1,29 @@
 class Solution {
-    private long[] multi = {2L, 3L, 5L};
-    
+  
+    public static Ugly ugly = new Ugly();
     public int nthUglyNumber(int n) {
-        PriorityQueue<Long> pq = new PriorityQueue<>();
-        Set<Long> s = new HashSet<>();
-        pq.offer(1L); s.add(1L);
-        
-        for(int i = 1 ; i < n ; ++i) {
-            long base = pq.poll();
-            for(int j = 0 ; j < 3 ; ++j) {
-                if(!s.contains(base*multi[j])) {
-                    pq.offer(base*multi[j]);
-                    s.add(base*multi[j]);
-                }
-            }
-        }
-        
-        return pq.poll().intValue();
+     
+      return ugly.nums[n-1];
+      
     }
+}
+
+class Ugly {
+  
+  int[] nums;
+  
+  public Ugly(){
+    nums = new int[1690];
+    nums[0] = 1;
+    int i2 = 0, i3 = 0, i5 = 0;
+    for(int i=1;i<1690;i++){
+      
+      nums[i] = Math.min(Math.min(nums[i2]*2, nums[i3]*3), nums[i5]*5);
+      
+      if(nums[i]==nums[i2]*2) i2++;
+      if(nums[i]==nums[i3]*3) i3++;
+      if(nums[i]==nums[i5]*5) i5++;
+    }
+  }
+  
 }
