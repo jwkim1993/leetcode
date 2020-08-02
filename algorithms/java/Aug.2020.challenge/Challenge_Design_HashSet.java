71f9f9184821aa57c5dd1586df75158ed005a9e7
JaewonKim <huhu420@naver.com>
2020-08-02 16:16:33 +0900

    [challenge] Add two challenge solutions
    - Add 1st and 2nd leetcode challenge solution
    - Add to README.md

---
+++ algorithms/java/Aug.2020.challenge/Challenge_Design_HashSet.java
@@ -0,0 +1,30 @@
+class MyHashSet {
+
+    boolean[] set;
+    
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+    
+    public void add(int key) {
+        set[key] = true;
+    }
+    
+    public void remove(int key) {
+        set[key] = false;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
